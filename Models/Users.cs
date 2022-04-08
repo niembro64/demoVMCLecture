258b70d0de95a03d4000d9f9803da7b759639897
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-04-08 11:52:25 -0400

    update

---
+++ Models/Users.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace demoVMCLecture.Models
+{
+  public class User{
+    public string Name {get;set;}
+    public string Color {get;set;}
+    public int Number {get;set;}
+  }
+}
